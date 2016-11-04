4d153d82a3277544177ceb86b509d800d54ef990
JoeChester <jzone123@googlemail.com>
2016-11-04 23:51:12 +0100

    added RESTful routes for demo Todo model

--- fuldaflats/server/endpoints.js
+++ fuldaflats/server/endpoints.js
@@ -10,23 +10,58 @@ app.use(bodyParser.json());
 //Endpoint Definitions
 app.get('/api/todos', function (req, res) {
     schema.models.Todo.find(function (err, todos) {
-        res.send(todos);
+        if (err != null) {
+            res.json(err);
+        } else {
+            res.json(todos);
+        }
     });
 });
 
 app.get('/api/todos/:todoId', function (req, res) {
-    schema.models.Todo.findById(req.params.todoId, function(err, todo){
-        res.json(todo);
+    schema.models.Todo.findById(req.params.todoId, function (err, todo) {
+        if (err != null) {
+            res.json(err);
+        } else {
+            res.json(todo);
+        }
     });
 });
-
 
 app.post('/api/todos', function (req, res) {
     schema.models.Todo.create(
         req.body,
         function (err, todo) {
-            res.json(todo);
+            if (err != null) {
+                res.json(err);
+            } else {
+                res.json(todo);
+            }
         });
+});
+
+app.put('/api/todos/:todoId', function (req, res) {
+    schema.models.Todo.updateOrCreate({
+            id: req.params.todoId
+        },
+        req.body,
+        function (err, todo) {
+            if (err != null) {
+                res.json(err);
+            } else {
+                res.json(todo);
+            }
+        });
+});
+
+app.delete('/api/todos/:todoId', function (req, res) {
+    schema.models.Todo.destroyById(req.params.todoId, function (err) {
+        if (err != null) {
+            res.json(err);
+        } else {
+            res.sendStatus(204);
+        }
+    });
 });
 
 //Static test_website
