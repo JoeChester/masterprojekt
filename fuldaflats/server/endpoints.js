13707e0c2d1e986e36e9497d481baa59e427dadf
JoeChester <jzone123@googlemail.com>
2016-11-05 15:38:08 +0100

    added express session tryouts for user login

--- fuldaflats/server/endpoints.js
+++ fuldaflats/server/endpoints.js
@@ -1,11 +1,23 @@
 //Require Setup
+var config = require('../config.json')[process.env.NODE_ENV || 'development'];
 var express = require('express');
 var schema = require('./models');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 //Express Setup
 var app = express();
 app.use(bodyParser.json());
+
+var sessionStore = new MySQLStore(config.db);
+app.use(session({
+    key: 'ff_session',
+    secret: 'fuldaflats2016',
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: true
+}));
 
 //Endpoint Definitions
 app.get('/api/todos', function (req, res) {
@@ -62,6 +74,15 @@ app.delete('/api/todos/:todoId', function (req, res) {
             res.sendStatus(204);
         }
     });
+});
+
+
+app.get('/api/views', function (req, res) {
+    if(req.session.views >= 0)
+        req.session.views ++;
+    else
+        req.session.views = 0;
+    res.json(req.session.views);
 });
 
 //Static test_website
