081233d4d3b9a23eb65361f4d9d659dcf48f3531
JoeChester <jzone123@googlemail.com>
2016-11-10 10:41:24 +0100

    debug migrate script I

--- fuldaflats/migrate.js
+++ fuldaflats/migrate.js
@@ -17,6 +17,7 @@ startup_msg();
 
 //Migrate Database
 console.log("Migrating Database Models...");
+console.log(schema.settings);
 schema.autoupdate(function(){
     console.log(chalk.blue("Done! :)"));
     process.exit(0);
