9df99f3673b4aaf945e527a89e1396bcb997f02a
JoeChester <jzone123@googlemail.com>
2016-11-10 14:23:56 +0100

    little change on bower install

--- fuldaflats/windows_installer.bat
+++ fuldaflats/windows_installer.bat
@@ -1,8 +1,10 @@
 WHERE bower
 IF %ERRORLEVEL% NEQ 0 npm install bower -g
 git pull
+ECHO Installing Node Depenencies..
 call npm install
 cd client
+ECHO Installing Bower Depenencies..
 call bower install
 cd ..
 call node migrate.js
