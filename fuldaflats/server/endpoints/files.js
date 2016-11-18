3615ef141621258230f3de3d9531ea144fd5fc9c
JoeChester <jzone123@googlemail.com>
2016-11-18 23:39:46 +0100

    errorhandling for fileupload

--- fuldaflats/server/endpoints/files.js
+++ fuldaflats/server/endpoints/files.js
@@ -3,30 +3,39 @@
  * Author:          Jonas Kleinkauf
  * LastMod:         18.11.2016
  * Description:     REST endpoints for fileupload
-************************************************************/
+ ************************************************************/
 
 var config = require('../../config.json')[process.env.NODE_ENV || 'development'];
 var express = require('express')
-var multer  = require('multer');
+var multer = require('multer');
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, config.express.fileUpload)
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
-  }
+    destination: function (req, file, cb) {
+        cb(null, config.express.fileUpload)
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
+    }
 })
 
 
-var upload = multer({ storage: storage });
+var upload = multer({
+    storage: storage
+}).single('file');
 var router = express.Router();
 
-router.post('/', upload.single('file'), function (req, res, next) {
-  let _filepath = {};
-  _filepath.path = config.express.fileDownload + req.file.filename;
-  res.status(201);
-  res.json(_filepath);
+router.post('/', function (req, res) {
+    upload(req, res, function (err) {
+        if(err){
+            res.status(400);
+            res.json(err);
+            return;
+        }
+        let _filepath = {};
+        _filepath.path = config.express.fileDownload + req.file.filename;
+        res.status(201);
+        res.json(_filepath);
+    });
 });
 
 module.exports = router;
