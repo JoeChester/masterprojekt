1a0a5005d4dd9f5ff2b8c88db70a917984b63af0
JoeChester <jzone123@googlemail.com>
2016-11-23 16:12:21 +0100

    added result page static template (proposal/searchResults)

--- fuldaflats/client/proposal/js/searchResults.js
+++ fuldaflats/client/proposal/js/searchResults.js
@@ -1,3 +1,5 @@
+var resultMap;
+
 function getSearchResults(){
     $.ajax({
         url: "/api/offers/search",
@@ -10,6 +12,16 @@ function getSearchResults(){
     });
 }
 
+function setupMap(){
+    resultMap = L.map('resultMap').setView([50.5647986, 9.6828528], 14);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    minZoom: 11,
+        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    }).addTo(resultMap);
+}
+
 $(function() {
+    setupMap();
     getSearchResults();
 });
