9711efd90eebd5cd530220e1214da62c4b0eb120
JoeChester <jzone123@googlemail.com>
2016-12-10 03:21:48 +0100

    added api endpoint DELETE /api/mediaObjects/:mediaObjectId to remove media objects from offer

--- fuldaflats/server/endpoints/mediaObjects.js
+++ fuldaflats/server/endpoints/mediaObjects.js
@@ -33,4 +33,35 @@ router.get('/:offerID', (req, res) => {
     }
 });
 
+//Delete mediaObject from offer. TODO: Remove file from hard drive... (PRIO2)
+router.delete('/:mediaObjectId', (req, res)=>{
+    if (!req.session.auth) {
+        return res.sendStatus(403);
+    }
+    //Check if MediaObject exists
+    schema.models.MediaObject.findById(req.params.mediaObjectId, (err, mo)=>{
+        if(err || !mo){
+            return res.sendStatus(404);
+        }
+        if(!mo.offerId){
+            return res.sendStatus(404); 
+        }
+        //Check if mediaObject belongs to an offer of the currently
+        //logged in user
+        schema.models.Offer.findById(mo.offerId, (err2, offer) =>{
+            if(offer.landlord != req.session.user.id){
+                return res.sendStatus(403);
+            }
+            //Everything ok, destroy mediaObject
+            mo.destroy((err) =>{
+                if(err){
+                    res.status(500);
+                    return res.json(err)
+                }
+                return res.sendStatus(204); 
+            })
+        })
+    })
+});
+
 module.exports = router;
